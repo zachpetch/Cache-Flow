127f41177471c2f093e1e61d2214a807714af572
Zach Petch <zachpetch@gmail.com>
2024-02-13 13:06:41 -0800

    Initial Commit

---
+++ Cache Flow/Cache_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cache_FlowApp.swift
+//  Cache Flow
+//
+//  Created by Zach Petch on 2024-02-13.
+//
+
+import SwiftUI
+
+@main
+struct Cache_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
